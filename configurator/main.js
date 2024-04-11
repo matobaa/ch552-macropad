d7765d5111d8a84c706385334baff693a23fc652
Akihiro MATOBA <matobaa+github@gmail.com>
2024-04-11 21:39:25 +0900

    add macro button

--- configurator/main.js
+++ configurator/main.js
@@ -4,7 +4,7 @@ window.onload = () => {
     const vendorId = 0x1189;
     const filters = [{vendorId}];
 
-    const sendReport = (reportId, data) => device.sendReport(reportId, Uint8Array.from(data));
+    const sendReport = async (reportId, data) => await device.sendReport(reportId, Uint8Array.from(data));
     document.querySelector("#connect").onclick = async event => {
         const devices = await navigator.hid.requestDevice({filters});
         device = devices.filter(device => device.collections.filter(u=>u.outputReports.length).length)[0];
@@ -13,4 +13,14 @@ window.onload = () => {
     }
     document.querySelector("#led1").onclick = event => sendReport(3, [0xb0, 0x18, 0x01]);
     document.querySelector("#led2").onclick = event => sendReport(3, [0xb0, 0x18, 0x02]);
+    document.querySelector("#macro").onclick = event => {
+        sendReport(3, [0xa1, 0x01]);
+        sendReport(3, [0x01, 0x11, 0x05]);
+        sendReport(3, [0x01, 0x11, 0x05, 0x01, 0x00, 0x04]);
+        sendReport(3, [0x01, 0x11, 0x05, 0x02, 0x00, 0x05]);
+        sendReport(3, [0x01, 0x11, 0x05, 0x03, 0x00, 0x06]);
+        sendReport(3, [0x01, 0x11, 0x05, 0x04, 0x00, 0x07]);
+        sendReport(3, [0x01, 0x11, 0x05, 0x05, 0x00, 0x08]);
+        sendReport(3, [0xaa, 0xaa]);
+    }
 }
