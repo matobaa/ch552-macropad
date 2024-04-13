d7d784928ce76b8a9a63fefe17785cecd2bf3a33
Akihiro MATOBA <matobaa+github@gmail.com>
2024-04-14 01:19:00 +0900

    add choices of keycode

--- configurator/main.js
+++ configurator/main.js
@@ -36,7 +36,7 @@ window.onload = () => {
                         <rect id=outer x=1 y=1 width=18 height=18 rx=1 ry=1 stroke-width=.5 fill=lightgrey></rect>
                         <rect id=inner x=3 y=2 width=14 height=13 rx=1.8 ry=1.8 stroke-width=.1 fill=white></rect>
                         <text x=5 y=8 font-size=5 stroke=none fill=black>${key}</text>
-                </svg>`;
+                    </svg>`;
                 key_area.appendChild(mxkey);
             } else if (Array.isArray(key)) { // Array assumes a rotary encoder
                 const rotaryencoder = document.createElement("span");
@@ -44,19 +44,27 @@ window.onload = () => {
                     <svg width=100 height=100 viewBox=0,0,20,22 fill=white
                             stroke=darkgrey stroke-width=.5 stroke-linecap=round stroke-linejoin=round pointer-events=bounding-box>
                         <path id=bottom d="m 1 12 a 9,9,0,0,0,18,0" fill=lightgrey></path>
-                    <ellipse id=outer cx=10 cy=10 rx=9 ry=9></ellipse>
+                        <ellipse id=outer cx=10 cy=10 rx=9 ry=9></ellipse>
                         <path id=key_${key.at(0)} d="m 8,4 a 6,6,0,0,0,-4,6 l-.5,-2 m1.5,.5 l-1,1.5" onclick=aim(${key.at(0)}) stroke=black></path desc=leftturn>
                         <path id=key_${key.at(2)} d="m 12,4 a 6,6,0,0,1,4,6 l.5,-2 m-1.5,.5 l1,1.5"  onclick=aim(${key.at(2)}) stroke=black></path desc=rightturn>
                         <path id=key_${key.at(1)} d="m 10,4 l 0,6,-1,-1 m 2,0 l -1,1 m2.5,0 l-5,0"   onclick=aim(${key.at(1)}) stroke=black></path desc=push>
-                    <text x=1.5  y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(0)})>${key.at(0)||""}</text>
-                    <text x=7.5  y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(1)})>${key.at(1)||""}</text>
-                    <text x=13.5 y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(2)})>${key.at(2)||""}</text>
+                        <text x=1.5  y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(0)})>${key.at(0)||""}</text>
+                        <text x=7.5  y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(1)})>${key.at(1)||""}</text>
+                        <text x=13.5 y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(2)})>${key.at(2)||""}</text>
                     </svg>`;
-                    key_area.appendChild(rotaryencoder);
+                key_area.appendChild(rotaryencoder);
             }
         });
         key_area.appendChild(document.createElement("br"));  // break at end of a row
     });
+
+    //
+    for (let i = 0; i < 5; i++) {
+        keycode = document.createElement("li");
+        keycode.id = `keycode_${i}`;
+        keycode.innerHTML = keycode_html(i);
+        keycodes.appendChild(keycode);
+    }
 }
 
 var target = undefined;
@@ -65,4 +73,17 @@ const aim = (key) => {
     target && target.classList.remove("target");
     target = document.querySelector(`#key_${key}`);
     target.classList.add("target");
+}
+
+const keycode_html = (i) => {
+    hid_keyboard_usage_table = [
+        // usage_id, usage_name, face
+        ["0x04", "Keyboard a and A", "aA"],
+        ["0x05", "Keyboard b and B", "bB"]
+    ];
+    return ["Shift", "Ctrl", "Alt", "Win"].map((mod) => `<label for=mod_${i}_${mod}><input type=checkbox id=mod_${i}_${mod}></input>${mod}</label>`).join(" ")
+         + ` + <select id=keycode_${i} size=1>`
+         + `<option value=NOP selected>(NOP)</option>`
+         + hid_keyboard_usage_table.map( ([usage_id, usage_name, face]) => `<option value=${usage_id}>${face}: ${usage_name}</option>`).join("\n")
+         + `</select>`;
 }
