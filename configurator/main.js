d2026d6dd8734e8c3dbbf24bc08b69a0ec5a4780
Akihiro MATOBA <matobaa+github@gmail.com>
2024-04-14 01:18:34 +0900

    refine target svg

--- configurator/main.js
+++ configurator/main.js
@@ -32,23 +32,22 @@ window.onload = () => {
             if (Number.isInteger(key)) {  // Integer assumes a cherrymx key
                 const mxkey = document.createElement("span");
                 mxkey.innerHTML = `
-                <svg width=95.25 height=95.25 viewBox=0,0,20,20 fill=ivory
-                        stroke=black stroke-width=.5 stroke-linecap=round stroke-linejoin=round
-                        onclick=aim(${key}); id=key_${key}>
-                    <rect id=outer x=1 y=1 width=18 height=18 rx=1 ry=1></rect>
-                    <rect id=inner x=3 y=2 width=14 height=13 rx=1 ry=1></rect>
-                    <text x=5 y=10 font-size=8 stroke=none fill=black>${key}</text>
+                    <svg width=95.25 height=95.25 viewBox=0,0,20,20 onclick=aim(${key}) id=key_${key} stroke=darkgrey>
+                        <rect id=outer x=1 y=1 width=18 height=18 rx=1 ry=1 stroke-width=.5 fill=lightgrey></rect>
+                        <rect id=inner x=3 y=2 width=14 height=13 rx=1.8 ry=1.8 stroke-width=.1 fill=white></rect>
+                        <text x=5 y=8 font-size=5 stroke=none fill=black>${key}</text>
                 </svg>`;
                 key_area.appendChild(mxkey);
             } else if (Array.isArray(key)) { // Array assumes a rotary encoder
                 const rotaryencoder = document.createElement("span");
                 rotaryencoder.innerHTML = `
-                <svg width=100 height=100 viewBox=0,0,20,20 fill=ivory
-                        stroke=black stroke-width=.5 stroke-linecap=round stroke-linejoin=round pointer-events=bounding-box>
+                    <svg width=100 height=100 viewBox=0,0,20,22 fill=white
+                            stroke=darkgrey stroke-width=.5 stroke-linecap=round stroke-linejoin=round pointer-events=bounding-box>
+                        <path id=bottom d="m 1 12 a 9,9,0,0,0,18,0" fill=lightgrey></path>
                     <ellipse id=outer cx=10 cy=10 rx=9 ry=9></ellipse>
-                    <path id=key_${key.at(0)} d="m 8,4 a 6,6,0,0,0,-4,6 l-.5,-2 m1.5,.5 l-1,1.5" onclick=aim(${key.at(0)})></path desc=leftturn>
-                    <path id=key_${key.at(2)} d="m 12,4 a 6,6,0,0,1,4,6 l.5,-2 m-1.5,.5 l1,1.5"  onclick=aim(${key.at(2)})></path desc=rightturn>
-                    <path id=key_${key.at(1)} d="m 10,4 l 0,6,-1,-1 m 2,0 l -1,1 m2.5,0 l-5,0"   onclick=aim(${key.at(1)})></path desc=push>
+                        <path id=key_${key.at(0)} d="m 8,4 a 6,6,0,0,0,-4,6 l-.5,-2 m1.5,.5 l-1,1.5" onclick=aim(${key.at(0)}) stroke=black></path desc=leftturn>
+                        <path id=key_${key.at(2)} d="m 12,4 a 6,6,0,0,1,4,6 l.5,-2 m-1.5,.5 l1,1.5"  onclick=aim(${key.at(2)}) stroke=black></path desc=rightturn>
+                        <path id=key_${key.at(1)} d="m 10,4 l 0,6,-1,-1 m 2,0 l -1,1 m2.5,0 l-5,0"   onclick=aim(${key.at(1)}) stroke=black></path desc=push>
                     <text x=1.5  y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(0)})>${key.at(0)||""}</text>
                     <text x=7.5  y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(1)})>${key.at(1)||""}</text>
                     <text x=13.5 y=14 font-size=3.6 stroke=none fill=black onclick=aim(${key.at(2)})>${key.at(2)||""}</text>
