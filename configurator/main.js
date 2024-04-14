52b953f0c829f95e9575c8e006c724d41a1378b9
Akihiro MATOBA <matobaa+github@gmail.com>
2024-04-15 02:45:06 +0900

    use querySelector instread of global id

--- configurator/main.js
+++ configurator/main.js
@@ -2,24 +2,16 @@ window.onload = () => {
     var device;
     
     const sendReport = async (reportId, data) => await device.sendReport(reportId, Uint8Array.from(data));
-    connect.onclick = async event => {
+    document.querySelector("#connect").addEventListener("click", async event => {
         const vendorId = parseInt(vid.innerText,16) || 0x1189;
         const devices = await navigator.hid.requestDevice({filters: [{vendorId}]});
         device = devices.filter(device => device.collections.filter(u=>u.outputReports.length).length)[0];
         await device.open();
-        event.srcElement.innerText = "connected";
-    }
-    led1.onclick = event => sendReport(3, [0xb0, 0x18, 0x01]);
-    led2.onclick = event => sendReport(3, [0xb0, 0x18, 0x02]);
-    download.onclick = event => {
-        sendReport(3, [0xa1, 0x01]);
-        sendReport(3, [0x01, 0x11, 0x05]);
-        sendReport(3, [0x01, 0x11, 0x05, 0x01, 0x00, 0x04]);
-        sendReport(3, [0x01, 0x11, 0x05, 0x02, 0x00, 0x05]);
-        sendReport(3, [0x01, 0x11, 0x05, 0x03, 0x00, 0x06]);
-        sendReport(3, [0x01, 0x11, 0x05, 0x04, 0x00, 0x07]);
-        sendReport(3, [0x01, 0x11, 0x05, 0x05, 0x00, 0x08]);
-        sendReport(3, [0xaa, 0xaa]);
+        event.target.innerText = "connected";
+    });
+    
+    document.querySelector("#led1").addEventListener("click", event => sendReport(3, [0xb0, 0x18, 0x01]));
+    document.querySelector("#led2").addEventListener("click", event => sendReport(3, [0xb0, 0x18, 0x02]));
     }
 
     // render a macropad
